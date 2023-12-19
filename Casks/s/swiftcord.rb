984f16e288e0091db24b7daa3212ef941aa501aa
Harry Lewandowski <harrylewandowski04@gmail.com>
2023-12-19 12:10:46 +1100

    swiftcord 0.7.1

--- Casks/s/swiftcord.rb
+++ Casks/s/swiftcord.rb
@@ -1,8 +1,8 @@
 cask "swiftcord" do
-  version "0.6.1"
-  sha256 "6da79f88930ce384a58c3fd902f75ad64ef0b9e015aa547db6a85af7d46b2032"
+  version "0.7.1"
+  sha256 "36d9f9fbed2ab3e124c69e7ef16b5f3f6979fa6c576e6ad005ff34c1738a2961"
 
-  url "https://github.com/SwiftcordApp/Swiftcord/releases/download/v#{version}/Swiftcord.#{version}.dmg"
+  url "https://github.com/SwiftcordApp/Swiftcord/releases/download/v#{version}/Swiftcord.zip"
   name "Swiftcord"
   desc "Native Discord client built in Swift"
   homepage "https://github.com/SwiftcordApp/Swiftcord"
